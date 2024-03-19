51244b19c1c07c8fc045a81558daaa7f4a46ca82
Yusuf U <devuzan@gmail.com>
2024-03-19 09:52:43 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,27 @@
-// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
     name: "SimpleTwoWayBinding",
     platforms: [
-        .iOS(.v10)
+      .iOS(.v13)
     ],
     products: [
-        .library(name: "SimpleTwoWayBinding", targets: ["SimpleTwoWayBinding"])
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SimpleTwoWayBinding",
+            targets: ["SimpleTwoWayBinding"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .target(name: "SimpleTwoWayBinding", path: "Sources/SimpleTwoWayBinding")
-    ]
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SimpleTwoWayBinding" path: "Sources/SimpleTwoWayBinding"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
 )
